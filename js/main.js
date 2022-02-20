bbcd4c43cddf890b1ccb71b641d598b1fdf72b79
赤石　昇太郎 <akaishi.shotaro17@gmail.com>
2022-02-20 17:34:55 +0900

    関数での処理

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 'use strict';
 
-for (let i = 1; i <= 10; i++) {
-//   if (i === 4)
-//   if (i % 3 === 0) {
-//     continue;
-// }
-if (i === 4){
-  break;
+function showAd() {
+  console.log('-----------');
+  console.log('-----Ad----');
+  console.log('-----------');
 }
-console.log(i)
-}
+
+showAd();
+console.log('Tom is great!');
+console.log('Bob is great!');
+showAd();
+console.log('Steve is great!');
+console.log('Richard is great!');
+showAd();

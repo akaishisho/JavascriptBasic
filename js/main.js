e86764bd917e0b78800285d1ee534e38463f1708
赤石　昇太郎 <akaishi.shotaro17@gmail.com>
2022-02-26 15:16:00 +0900

    classNameの操作

--- js/main.js
+++ js/main.js
@@ -3,11 +3,7 @@
 {
     document.querySelector('button').addEventListener('click', () => {
       const targetNode =  document.getElementById('target');
-      // スタイルに関しては、 JavaScript でこのように書いてしまうと、 CSS との役割分担があいまいになるので、
-      // 見た目の指定は CSS に任せて、 JavaScript では class 属性の操作だけを書く方法が一般的。
-      targetNode.textContent = 'Changed!';
-      targetNode.title = 'This is title';
-      targetNode.style.color = 'red';
-      targetNode.style.backgroundColor = 'skyblue';
+
+      targetNode.className = 'my-color my-border';
     });
 }
